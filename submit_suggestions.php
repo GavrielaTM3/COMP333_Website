644edfa2ca78159f85f78a4989a6e5577099df05
Conrad <conrad.f.lanzoni@gmail.com>
2025-03-06 16:34:41 -0500

    added suggestion to homepage (Issue #66)

--- submit_suggestions.php
+++ submit_suggestions.php
@@ -15,13 +15,13 @@ session_start();
     <div class="suggest">  <!-- Keeping this div for styling -->
         <h2>Submit Your Suggestions</h2>
         <form action="process_suggestions.php" method="POST">
-            <label for="name">Name (Optional):</label>
-            <input type="text" id="name" name="name">
+            <label for="Coding Concept">Coding Concept</label>
 
-            <label for="suggestion">Your Suggestion:</label>
+            <textarea id="suggestion" name="theme" required></textarea>
+            <label for="Theme">Theme: </label>
+
             <textarea id="suggestion" name="suggestion" required></textarea>
-
-            <button type="submit">Submit</button>
+            <button type="submit">Submit: </button>
         </form>
     </div>
 
