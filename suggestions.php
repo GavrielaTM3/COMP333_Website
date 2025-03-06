6353429834dc666dbb5b884c6b475b0a2c7ccf5d
Gavriela Meyers <gmeyers@wesleyan.edu>
2025-03-06 15:09:12 -0500

    fixing suggest.php

--- suggestions.php
+++ suggestions.php
@@ -1,25 +1,5 @@
 <?php
 session_start();
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "app-db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Fetch suggestions
-$sql = "SELECT name, suggestion FROM suggestions ORDER BY id DESC";
-$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +12,7 @@ $result = $conn->query($sql);
 </head>
 <body>
 <div class="suggest">
-    <h2>Submit Your Suggestions Here!!</h2>
+    <h2>What lesson would you like to see on our website?</h2>
     <form action="process_suggestions.php" method="POST">
         <label for="name">Name (Optional):</label>
         <input type="text" id="name" name="name">
@@ -43,33 +23,5 @@ $result = $conn->query($sql);
         <button type="submit">Submit</button>
     </form>
 </div>
-
-<!-- Display Submitted Suggestions -->
-<div class="suggestions-list">
-    <h2>Previous Suggestions</h2>
-    <table border="1">
-        <tr>
-            <th>Name</th>
-            <th>Suggestion</th>
-        </tr>
-        <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . (!empty($row["name"]) ? $row["name"] : "Anonymous") . "</td>";
-                echo "<td>" . $row["suggestion"] . "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='2'>No suggestions yet.</td></tr>";
-        }
-        ?>
-    </table>
-</div>
-
 </body>
 </html>
-
-<?php
-$conn->close();
-?>
