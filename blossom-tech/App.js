13ffea5dc6d5e1eaed9bce62a3b9355a796d47ae
Gavriela Meyers <gmeyers@wesleyan.edu>
2025-04-08 15:53:11 -0400

    correct directory

--- blossom-tech/App.js
+++ blossom-tech/App.js
@@ -1,28 +1,33 @@
-// App.js
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import Suggestions from './pages/ViewSuggestions';
 
 export default function App() {
-  const [screen, setScreen] = useState('home'); 
+  const [screen, setScreen] = useState('home');
 
   return (
     <View style={styles.container}>
       {screen === 'home' && (
-        <Home onNavigateToLogin={() => setScreen('login')} />
+        <Home
+          onNavigateToLogin={() => setScreen('login')}
+          onNavigateToSuggestions={() => setScreen('suggestions')}
+        />
       )}
       {screen === 'login' && (
-  <Login
-    onNavigateBack={() => setScreen('home')}
-    onNavigateToRegister={() => setScreen('register')}
-  />
-)}
+        <Login
+          onNavigateBack={() => setScreen('home')}
+          onNavigateToRegister={() => setScreen('register')}
+        />
+      )}
       {screen === 'register' && (
         <Register onNavigateBack={() => setScreen('login')} />
-      )}    
+      )}
+      {screen === 'suggestions' && (
+        <Suggestions onNavigateBack={() => setScreen('home')} />
+      )}
     </View>
   );
 }
