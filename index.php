60c16dc5d99d6c6025b06bdd32c6156c1e72eb51
Gavriela Meyers <gmeyers@wesleyan.edu>
2025-04-27 20:28:31 -0400

    Complete BlossomTech redesign: dark mode and new layout

--- index.php
+++ index.php
@@ -1,123 +1,105 @@
 <?php
-session_start(); // Start session
+session_start();
 
 // Check if user is logged in
 $is_logged_in = isset($_SESSION['username']);
 ?>
 
 <!DOCTYPE html>
-<meta name="description" content="Website that teaches you how to code based on your interests.">
 <html lang="en">
- 
-  <head>
-   
+<head>
     <meta charset="utf-8" />
-    
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="" />
-   
-    <title>Blossom Tech</title>
-   
-    <link rel="stylesheet" href="style.css" />
-  </head>
+    <meta name="description" content="Website that teaches you how to code based on your interests." />
+    <title>BlossomTech</title>
+    <link rel="stylesheet" href="style.css?v=1">
+</head>
 
- <!-- Nav bar --> 
 <body>
-<div id="navbar" class="navbar">
-<?php if ($is_logged_in): ?>
-  <li><a href="view_suggestions.php">Suggestions</a></li> 
-        <?php else: ?>
-        <?php endif; ?>
-    <ul>
-      <li><a href="#our-mission">Our Mission</a></li>
-      <li><a href="#testimonials">Testimonials</a></li>
-      <li><a href="#start-coding">Start Coding</a></li>
-    </ul>
-    <?php if ($is_logged_in): ?>
-            <li style="color: #BED8D4;">Logged in as: <?php echo htmlspecialchars($_SESSION['username']); ?></li>
-            <li><a href="logout.php">Log Out</a></li>
-        <?php else: ?>
-            <li><a href="login.php" class="login-button">Login</a></li>
-        <?php endif; ?>
-  
-  </div>
- 
+    <!-- Navigation Bar -->
+    <div id="navbar" class="navbar">
+        <ul>
+            <?php if ($is_logged_in): ?>
+                <li><a href="view_suggestions.php">Suggestions</a></li>
+            <?php endif; ?>
 
-<!-- Mission box --> 
-  <div class="mission-text"> 
-    <section id="our-mission"> 
-    <h1 id="welcome">Welcome to BlossomTech</h1>
-    <p > Our mission is to teach you how to code based on your interests! You will have a lot of fun coding and see that it is not that hard. Best of all, our website is completely FREE because we believe everyone, regardless of their background, should have the opportunity to learn how to code</p>
+            <li><a href="#our-mission">Our Mission</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#start-coding">Start Coding</a></li>
 
-  </div>
+            <?php if ($is_logged_in): ?>
+                <li style="color: #BED8D4;">Logged in as: <?php echo htmlspecialchars($_SESSION['username']); ?></li>
+                <li><a href="logout.php">Log Out</a></li>
+            <?php else: ?>
+                <li><a href="login.php" class="login-button">Login</a></li>
+            <?php endif; ?>
+        </ul>
+    </div>
 
-    <div class="main-content">
-   
-        <h1 class="start-coding">Start your first coding lesson for FREE!</h1>
-        <div class="container">
-            <a href="#" class="box">
-                <img src="https://cdn.pixabay.com/photo/2013/07/18/10/55/dna-163466_1280.jpg" alt="DNA">
-                <h3>Biology</h3>
-            </a>
-            
-            <a href="fashion_lesson.php" class="box">
-                <img src="https://cdn.pixabay.com/photo/2022/08/25/23/06/woman-7411414_1280.png" alt="Dress">
-                <h3>Fashion</h3>
-            </a>
-            
-            <a href="sports.php" class="box">
-                <img src="https://cdn.pixabay.com/photo/2025/01/28/09/32/rocket-9365225_1280.jpg" alt="Tennis">
-                <h3>Sports</h3>
-            </a>
-            
-            <a href="#" class="box">
-                <img src="https://cdn.pixabay.com/photo/2014/12/29/08/29/lens-582605_1280.jpg" alt="Lens">
-                <h3>Photography</h3>
-            </a>
+    <!-- Mission Section -->
+    <div class="mission-text" id="our-mission">
+        <h1>Welcome to BlossomTech</h1>
+        <p>Our mission is to teach you how to code based on your interests! You will have a lot of fun coding and see that it is not that hard. Best of all, it's completely FREE because we believe everyone, regardless of background, should have the opportunity to learn how to code.</p>
+    </div>
+
+    <!-- Grid Container for Lessons -->
+    <div class="grid-container" id="start-coding">
+        <a href="biology_lesson.php" class="box">
+            <img src="https://cdn.pixabay.com/photo/2013/07/18/10/55/dna-163466_1280.jpg" alt="Biology Image">
+            <h3>Biology</h3>
+        </a>
+
+        <a href="fashion_lesson.php" class="box">
+            <img src="https://cdn.pixabay.com/photo/2022/08/25/23/06/woman-7411414_1280.png" alt="Fashion Image">
+            <h3>Fashion</h3>
+        </a>
+
+        <a href="sports_lesson.php" class="box">
+            <img src="https://cdn.pixabay.com/photo/2025/01/28/09/32/rocket-9365225_1280.jpg" alt="Sports Image">
+            <h3>Sports</h3>
+        </a>
+
+        <a href="photography_lesson.php" class="box">
+            <img src="https://cdn.pixabay.com/photo/2014/12/29/08/29/lens-582605_1280.jpg" alt="Photography Image">
+            <h3>Photography</h3>
+        </a>
+    </div>
+
+    <!-- Testimonials Section -->
+    <h2 class="testimonials-title" id="testimonials">What people are saying...</h2>
+
+    <div class="testimonials">
+        <div class="testimonial-card">
+            <img src="https://images.pexels.com/photos/6248443/pexels-photo-6248443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Diana">
+            <div class="name">Diana Spenser</div>
+            <div class="role">Grandmother</div>
+            <p>"I am retired and wanted to learn how to code to help my grandkids with their homework."</p>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="https://images.pexels.com/photos/8421989/pexels-photo-8421989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Richard">
+            <div class="name">Richard Montero</div>
+            <div class="role">Kindergarten Student</div>
+            <p>"I learned to code to make video games that I now play with my friends."</p>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="https://images.pexels.com/photos/8124235/pexels-photo-8124235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Sonia">
+            <div class="name">Sonia Ramirez</div>
+            <div class="role">CEO</div>
+            <p>"After launching my startup, I wanted to learn to code so I could help out my web development team."</p>
         </div>
     </div>
-    <!-- Testimonials --> 
-    <div> 
-      <section id="testimonials"> 
-      <h1 class="testimonials-title">What people are saying...</h1>
-    
-    
+
+    <!-- Sample Coding Challenge Section -->
+    <div class="coding-section">
+        <h2>Try a Sample Coding Challenge!</h2>
+        <iframe src="sample_lesson.html" title="Sample Coding Challenge"></iframe>
     </div>
 
-    <div class="testimonials">
-      <img src="https://images.pexels.com/photos/6248443/pexels-photo-6248443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Avatar" >
-      <div class="testimonial-text">
-      <p><span>Diana Spenser</span> Grandmother</p>
-      <p>"I am retired and wanted to learn how to code to help my grandkids with their homework."</p>
-    </div>
-  </div>
-
-    <div class="testimonials">
-      <img src=https://images.pexels.com/photos/8421989/pexels-photo-8421989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Avatar" >
-      <div class="testimonial-text">
-      <p><span>Richard Montero</span> Kindergarten student</p>
-      <p>"I learned to code to make video games that I now play with my friends."</p>
-    </div>
-  </div>
-
-    <div class="testimonials">
-      <img src="https://images.pexels.com/photos/8124235/pexels-photo-8124235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Avatar">
-      <div class="testimonial-text">
-      <p><span>Sonia Ramirez</span> CEO</p>
-      <p>"After launching my startup, I wanted to learn to code so I could help out my web development team."</p>
-    </div>
-  </div>
-
-    <section id="start-coding"> 
-
-    <div class="iframe-container">
-      <div class="iframe-title">
-        <p id="challenge_title">Check out this sample coding challenge:</p></div>
-          <iframe src="sample_lesson.html" width="600" height="400" style="border:1px solid #ccc;" title="Sample Coding Challenge"></iframe>
-    </div>
-
+    <!-- Footer -->
+    <footer>
+        <p>This website was created for COMP333: Software Engineering at Wesleyan University as an exercise.</p>
+    </footer>
 </body>
-
-<footer>
-  <p>This is a website created for COMP333: Software Engineering at Wesleyan University. This is an exercise.</p>
-</footer>
+</html>
